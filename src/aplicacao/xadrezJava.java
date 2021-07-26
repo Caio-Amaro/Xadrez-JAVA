bb7b3048d3711a5499ec6ae79f6721e7a93f7e39
Caio Costa Amaro <amaro.caio@gmail.com>
2021-07-26 15:22:15 -0300

    Criação da primeira classe - posicao -

---
+++ src/aplicacao/xadrezJava.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import tabuleirojogo.Posicao;
+
+
+public class xadrezJava {
+    public static void main(String[] args) {
+        
+        Posicao posi = new Posicao(2, 7);
+        
+        System.out.print(posi);
+        
+    }
+    
+}
